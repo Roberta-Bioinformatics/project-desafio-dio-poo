04b7f2a9c02174ac37c30942cee6662b403ae558
Roberta-Bioinformatics <sroberta.biomedicina@gmail.com>
2023-01-01 07:12:37 -0300

    FinalTest projeto POO1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
+import br.com.dio.desafio.dominio.Bootcamp;
+import br.com.dio.desafio.dominio.Conteudo;
 import br.com.dio.desafio.dominio.Curso;
 import br.com.dio.desafio.dominio.Mentoria;
+import br.com.dio.desafio.dominio.Dev;
 import java.time.LocalDate;
 
 public class Main {
@@ -14,14 +17,66 @@ public class Main {
         curso2.setDescricao("descricao curso js");
         curso2.setCargaHoraria(4);
 
+        // **********************************************************************************************************************
+        // Conteudo conteudo = new Curso();
+        // tudo que tenho em conteudo tenho em curso.
+        // mas nem tudo que tenho em curso tenho em conteudo...
+        // *** Conceito de polimorfismo. ***
+
+        // classe Conteudo é classe mãe e abstrata para que se possa evitar a repetição de código...
+        // *** Conceito de herança --> classe Conteudo é mãe de curso e mentoria. ***
+
+        // Conteudo conteudo1 = new Mentoria();
+        // *** Conceito de polimorfismo --> objeto de diversas formas... pode instanciar mentoria atraves da classe conteudo porque
+        // conteudo é mãe de mentoria. Mas conteudo não pode dar um new em conteudo, porque é uma classe abstrata. ***
+        // **********************************************************************************************************************
+
         Mentoria mentoria = new Mentoria();
         mentoria.setTitulo("mentoria de java");
         mentoria.setDescricao("descrição mentoria java");
         mentoria.setData(LocalDate.now());
 
-        System.out.println(curso1);
+        // Para não se tornar repetitivo.
+        /*System.out.println(curso1);
         System.out.println(curso2);
-        System.out.println(mentoria);
+        System.out.println(mentoria);*/
+
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Java Developer");
+        bootcamp.setDescricao("Descrição Bootcamp Java Developer");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria);
+
+        Dev devTeacher = new Dev();
+        devTeacher.setNome("Camila");
+        devTeacher.inscreverBootcamp(bootcamp);
+        System.out.println("Conteudos Inscritos Teacher" + devTeacher.getConteudosInscritos());
+        devTeacher.progredir();
+        devTeacher.progredir();
+        devTeacher.progredir();
+        System.out.println("-");
+        System.out.println("Conteudos Inscritos Teacher" + devTeacher.getConteudosInscritos());
+        System.out.println("Conteudos Concluídos Teacher" + devTeacher.getConteudosConcluidos());
+        System.out.println("XP: " + devTeacher.calcularTotalXp());
+        System.out.println(" ");
+
+        System.out.println("ƸӜƷ (◡‿◡) ¯`•.¸¸.•´´¯`•• .¸¸.•´¯ ƸӜƷ");
+
+        System.out.println(" ");
+        Dev devAluno1 = new Dev();
+        devAluno1.setNome("Aluno1");
+        devAluno1.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos Inscritos Aluno_1" + devAluno1.getConteudosInscritos());
+        devAluno1.progredir();
+        devAluno1.progredir();
+        //devJoao.progredir();
+        System.out.println("-");
+        System.out.println("Conteúdos Inscritos Aluno_1" + devAluno1.getConteudosInscritos());
+        System.out.println("Conteúdos Concluídos Aluno_1" + devAluno1.getConteudosConcluidos());
+        System.out.println("XP: " + devAluno1.calcularTotalXp());
+        System.out.println(" ");
+        System.out.println("ƸӜƷ Gratidão! ƸӜƷ");
     }
 
 }
