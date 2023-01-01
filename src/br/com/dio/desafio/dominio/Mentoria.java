620f0a838f4a81a8ff846ce24ae7684cce127ce0
Roberta-Bioinformatics <sroberta.biomedicina@gmail.com>
2023-01-01 08:29:07 -0300

    FinalTest projeto POO1

--- src/br/com/dio/desafio/dominio/Mentoria.java
+++ src/br/com/dio/desafio/dominio/Mentoria.java
@@ -3,39 +3,34 @@ package br.com.dio.desafio.dominio;
 import java.time.LocalDate;
 
 public class Mentoria extends Conteudo{
-    //private String titulo;  /*já tem titulo e descrição na classe mãe*/
+    //private String titulo;  /*já tem titulo e descrição na classe mãe
     //private String descricao;//
     private LocalDate data;
 
     @Override
     public double calcularXp() {
+
         return XP_PADRAO + 20d;
     }
 
     public Mentoria() {
     }
 
-    //public String getTitulo() {
-    //    return titulo;
-    //}
+    //public String getTitulo() {return titulo;}
 
-    //public void setTitulo(String titulo) {
-    //    this.titulo = titulo;
-    //}
+    //public void setTitulo(String titulo) {this.titulo = titulo//}
 
-    //public String getDescricao() {
-    //    return descricao;
-    //}
+    //public String getDescricao() {return descricao;}
 
-    //public void setDescricao(String descricao) {
-    //    this.descricao = descricao;
-    //}
+    //public void setDescricao(String descricao) {this.descricao = descricao;}
 
     public LocalDate getData() {
+
         return data;
     }
 
     public void setData(LocalDate data) {
+
         this.data = data;
     }
 
@@ -44,7 +39,7 @@ public class Mentoria extends Conteudo{
         return "Mentoria{" +
                 "titulo='" + getTitulo() + '\'' +
                 ", descricao='" + getDescricao() + '\'' +
-                ", data=" + data +
+                ", data=" + getData() +
                 '}';
     }
 }
